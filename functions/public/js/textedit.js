061b1f79d04c4f5b3a5e4efc988dc5ac6e87c399
Jose Vigil <jose.vigil@roamtouch.com>
2020-01-23 06:34:25 -0300

    DataTable edits, selecciono la lista y muestra el texto a editar.

--- functions/public/js/textedit.js
+++ functions/public/js/textedit.js
@@ -37,245 +37,262 @@ $(document).ready(function () {
             homeLoader.hide();
 
           }       
-          count++;        
+          count++;      
 
-          /*collection.get().then(function (documentSnapshots) {
-            documentSnapshots.docs.forEach(doc => {
-        
-          
-              });
-          });*/
-        
       });   
+
+      $('#edit-list li').click(function() { 
+        $('li.list-group-item.active').removeClass("active"); 
+        $(this).addClass("active"); 
+      });
       
     }         
   
+    window.loadEditsList = function(_id) {
+
+      var _doc = editObjects[_id];   
   
-    function loadEdits(jsonData) { 
+      _doc.ref.collection("collection").get()
+      .then(function(querySnapshotEdit) {
+  
+           var editLength = querySnapshotEdit.docs.length;
+  
+           var countLines = 1;
+  
+           var jsonData = [];
+
+           var datatable = $('#grid-details').DataTable( {
+            "scrollY":        "150px",
+            "scrollCollapse": true,
+            "paging":         false,
+            "retrieve": true, 
+            "bInfo" : false,             
+            "buttons": [
+              'copy', 'excel', 'pdf'
+            ],
+            "columns": [
+              { "data": "meta.database_ref" },
+              { "data": "meta.Nombre" },
+              { "data": "meta.Actualizado" }
+            ],
+            "columnDefs": [
+              {
+                "targets": [ 0 ],
+                "visible": false,
+                "searchable": false
+              },{
+                  "targets": [ 1 ],
+                  "visible": true,
+                  "searchable": true
+              }
+            ]
+          });     
+          
+          $('#grid-details tbody').on('click', 'tr', function () {
+              var data = datatable.row( this ).data();
+              //alert( 'You clicked on '+data.meta.id+'\'s row' );
+              if ( $(this).hasClass('selected') ) {
+                  $(this).removeClass('selected');
+              }
+              else {
+                  datatable.$('tr.selected').removeClass('selected');
+                  $(this).addClass('selected');
+              }
+              loadEdits(data.meta.database_ref);
+          } );       
+          
+  
+           querySnapshotEdit.forEach(function(docEdit) {
+  
+             var editData = docEdit.data();     
+
+             var last_update = editData.last_update;
+             
+              var _date = last_update.toDate();
+
+              var year = _date.getFullYear();
+              var month = _date.getMonth()+1;
+              var day = _date.getDate();
+
+              if (day < 10) {
+                day = '0' + day;
+              }
+              if (month < 10) {
+                month = '0' + month;
+              }
+
+              var formattedDate = day + '-' + month + '-' + year
+  
+             let row = { "meta": {  "database_ref": editData.database_ref, "Nombre": editData.name, "Actualizado": formattedDate} };   
+  
+             jsonData.push(row);
+  
+             let database_ref = editData.database_ref;    
+             
+             if (editLength == countLines) {                               
+                
+                datatable.clear();
+                datatable.rows.add(jsonData);
+                datatable.draw();    
+             }
+             
+             countLines++;
+         });
+  
+       });     
+       
+     }
+  
+    function loadEdits(database_ref) { 
     
       var userId = _user.userId;
   
       var firepad = null, userList = null, codeMirror = null;
   
-      //function joinFirepadForHash() {
-  
-        $('#edit-list li').click(function() { 
-          $('li.list-group-item.active').removeClass("active"); 
-          $(this).addClass("active"); 
-        });       
-  
-        
-  
-        //var table = $("#grid-edits").DataTable();
-        //table.clear();
-  
-        if (firepad) {
-          // Clean up.
-          firepad.dispose();
-          userList.dispose();
-          $('.CodeMirror').remove();
-        }
-  
-        //var id = window.location.hash.replace(/#/g, '') || randomString(10);
-        var url = window.location.toString().replace(/#.*/, '') + '#' + id;
-        //var firepadRef = new Firebase('https://firepad.firebaseio.com/demo').child(id);
-  
-        var id =  _user.userId;
-        var firepadRef = database.ref(id);
-  
-        //var id = window.location.hash.replace(/#/g, '') || randomString(10);
-        var url = window.location.toString().replace(/#.*/, '') + '#' + id;
-        var firepadRef = new Firebase('https://firepad.firebaseio.com/demo').child(id);
-  
-        var id = window.location.hash.replace(/#/g, '') || randomString(10);
-        //var url = window.location.toString().replace(/#.*/, '') + '#' + id;
-        var firepadRef = new Firebase(firebaseConfig.databaseURL).child(id);
-  
-        var userId = firepadRef.push().name(); // Just a random ID.
-        codeMirror = CodeMirror(document.getElementById('firepad'), { lineWrapping: true });
-        firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
-            { richTextToolbar: true, richTextShortcuts: true, userId: userId});
-        userList = FirepadUserList.fromDiv(firepadRef.child('users'),
-            document.getElementById('firepad-userlist'), userId);
-  
-          firepad.on('ready', function() {
-            if (firepad.isHistoryEmpty()) {
-              firepad.setText('Welcome to your own private pad!\n\nShare the URL below and collaborate with your friends.');
-            }
-  
-            ensurePadInList(id);
-            buildPadList();
-          });
-  
-  
-          codeMirror.focus();
-  
-          window.location = url;
-          $('#url').val(url);
-          $("#url").on('click', function(e) {
-            $(this).focus().select();
-            e.preventDefault();
-            return false;
-          });
-  
-          setTimeout(function() {
-            $(window).on('hashchange', joinFirepadForHash);
-          }, 0);
-  
-        //}
-  
-        function padListEnabled() {
-          return (typeof localStorage !== 'undefined' && typeof JSON !== 'undefined' && localStorage.setItem &&
-              localStorage.getItem && JSON.parse && JSON.stringify);
-        }
-  
-        function ensurePadInList(id) {
-          if (!padListEnabled()) { return; }
-          var list = JSON.parse(localStorage.getItem('demo-pad-list') || "{ }");
-          if (!(id in list)) {
-            var now = new Date();
-            var year = now.getFullYear(), month = now.getMonth() + 1, day = now.getDate();
-            var hours = now.getHours(), minutes = now.getMinutes();
-            if (hours < 10) { hours = '0' + hours; }
-            if (minutes < 10) { minutes = '0' + minutes; }
-  
-            list[id] = [year, month, day].join('/') + ' ' + hours + ':' + minutes;
-  
-            localStorage.setItem('demo-pad-list', JSON.stringify(list));
-            buildPadList();
-          }
-        }
-  
-        function buildPadList() {
-          if (!padListEnabled()) { return; }
-          $('#my-pads-list').empty();
-  
-          var list = JSON.parse(localStorage.getItem('demo-pad-list') || '{ }');
-          for(var id in list) {
-            $('#my-pads-list').append(
-                $('<div></div>').addClass('my-pads-item').append(
-                    makePadLink(id, list[id])
-            ));
-          }
-        }
-  
-        function makePadLink(id, name) {
-          return $('<a></a>')
-              .text(name)
-              .on('click', function() {
-                window.location = window.location.toString().replace(/#.*/, '') + '#' + id;
-                $('#my-pads-list').hide();
-                return false;
-          });
-        }
-  
-        function randomString(length) {
-          var text = "";
-          var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  
-          for(var i=0; i < length; i++)
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-  
-          return text;
-        }
-  
-        /*$(window).on('ready', function() {
-          joinFirepadForHash();
-          setTimeout(function() {
-            $(window).on('hashchange', joinFirepadForHash);
-          }, 0);
-        });*/
-  
-        function displayPads() {
-          $('#my-pads-list').toggle();
-        }
-  
-        function getFirepadById (firepadId) {
-  
-            var getFirePadFromRef = functions.httpsCallable('getFirePadFromRef');
-            getFirePadFromRef({"refId": firepadId}).then(function(result) {
-            
-            var sanitizedMessage = result.data.text;
-  
-            console.log(sanitizedMessage);
-            
-          }).catch(function(error) {
-            // Getting the Error details.
-            var code = error.code;
-            var message = error.message;
-            var details = error.details;
-            // ...
-          });
-  
-        }
-  
+      if (firepad) {
+        // Clean up.
+        firepad.dispose();
+        userList.dispose();
+        $('.CodeMirror').remove();
       }
+
+      //var id = window.location.hash.replace(/#/g, '') || randomString(10);
+      var url = window.location.toString().replace(/#.*/, '') + '#' + id;
+      //var firepadRef = new Firebase('https://firepad.firebaseio.com/demo').child(id);
+
+      var id =  _user.userId;
+      var firepadRef = database_ref; //database.ref(id);
+
+      //var id = window.location.hash.replace(/#/g, '') || randomString(10);
+      var url = window.location.toString().replace(/#.*/, '') + '#' + id;
+      var firepadRef = new Firebase('https://firepad.firebaseio.com/demo').child(id);
+
+      var id = window.location.hash.replace(/#/g, '') || randomString(10);
+      //var url = window.location.toString().replace(/#.*/, '') + '#' + id;
+      var firepadRef = new Firebase(firebaseConfig.databaseURL).child(id);
+
+      var userId = firepadRef.push().name(); // Just a random ID.
+      codeMirror = CodeMirror(document.getElementById('firepad'), { lineWrapping: true });
+      firepad = Firepad.fromCodeMirror(firepadRef, codeMirror,
+          { richTextToolbar: true, richTextShortcuts: true, userId: userId});
+      userList = FirepadUserList.fromDiv(firepadRef.child('users'),
+          document.getElementById('firepad-userlist'), userId);
+
+        firepad.on('ready', function() {
+          if (firepad.isHistoryEmpty()) {
+            firepad.setText('Welcome to your own private pad!\n\nShare the URL below and collaborate with your friends.');
+          }
+
+          ensurePadInList(id);
+          buildPadList();
+        });
+
+
+        codeMirror.focus();
+
+        window.location = url;
+        $('#url').val(url);
+        $("#url").on('click', function(e) {
+          $(this).focus().select();
+          e.preventDefault();
+          return false;
+        });
+
+        setTimeout(function() {
+          $(window).on('hashchange', joinFirepadForHash);
+        }, 0);
+
+      //}
+
+      function padListEnabled() {
+        return (typeof localStorage !== 'undefined' && typeof JSON !== 'undefined' && localStorage.setItem &&
+            localStorage.getItem && JSON.parse && JSON.stringify);
+      }
+
+      function ensurePadInList(id) {
+        if (!padListEnabled()) { return; }
+        var list = JSON.parse(localStorage.getItem('demo-pad-list') || "{ }");
+        if (!(id in list)) {
+          var now = new Date();
+          var year = now.getFullYear(), month = now.getMonth() + 1, day = now.getDate();
+          var hours = now.getHours(), minutes = now.getMinutes();
+          if (hours < 10) { hours = '0' + hours; }
+          if (minutes < 10) { minutes = '0' + minutes; }
+
+          list[id] = [year, month, day].join('/') + ' ' + hours + ':' + minutes;
+
+          localStorage.setItem('demo-pad-list', JSON.stringify(list));
+          buildPadList();
+        }
+      }
+
+      function buildPadList() {
+        if (!padListEnabled()) { return; }
+        $('#my-pads-list').empty();
+
+        var list = JSON.parse(localStorage.getItem('demo-pad-list') || '{ }');
+        for(var id in list) {
+          $('#my-pads-list').append(
+              $('<div></div>').addClass('my-pads-item').append(
+                  makePadLink(id, list[id])
+          ));
+        }
+      }
+
+      function makePadLink(id, name) {
+        return $('<a></a>')
+            .text(name)
+            .on('click', function() {
+              window.location = window.location.toString().replace(/#.*/, '') + '#' + id;
+              $('#my-pads-list').hide();
+              return false;
+        });
+      }
+
+      function randomString(length) {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for(var i=0; i < length; i++)
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+      }
+
+      /*$(window).on('ready', function() {
+        joinFirepadForHash();
+        setTimeout(function() {
+          $(window).on('hashchange', joinFirepadForHash);
+        }, 0);
+      });*/
+
+      function displayPads() {
+        $('#my-pads-list').toggle();
+      }
+
+      function getFirepadById (firepadId) {
+
+          var getFirePadFromRef = functions.httpsCallable('getFirePadFromRef');
+          getFirePadFromRef({"refId": firepadId}).then(function(result) {
+          
+          var sanitizedMessage = result.data.text;
+
+          console.log(sanitizedMessage);
+          
+        }).catch(function(error) {
+          // Getting the Error details.
+          var code = error.code;
+          var message = error.message;
+          var details = error.details;
+          // ...
+        });
+
+      }
+
+    }
       
 
-      window.loadEditsList = function(_id) {
-
-        var _doc = editObjects[_id];   
-    
-        _doc.ref.collection("collection").get()
-        .then(function(querySnapshotEdit) {
-    
-             var editLength = querySnapshotEdit.docs.length;
-    
-             var countLines = 1;
-    
-             var jsonData = [];
-
-             var datatable = $('#grid-details').DataTable( {
-              "scrollY":        "150px",
-              "scrollCollapse": true,
-              "paging":         false,
-              //"data": jsonData,
-              "columns": [
-                { "data": "meta.Nombre" },
-                { "data": "meta.Actualizado" }
-              ]
-            });
-    
-             querySnapshotEdit.forEach(function(docEdit) {
-    
-               var editData = docEdit.data();     
-
-               var last_update = editData.last_update;
-               
-                var _date = last_update.toDate();
-
-                var year = _date.getFullYear();
-                var month = _date.getMonth()+1;
-                var day = _date.getDate();
-
-                if (day < 10) {
-                  day = '0' + day;
-                }
-                if (month < 10) {
-                  month = '0' + month;
-                }
-
-                var formattedDate = day + '-' + month + '-' + year
-    
-               let row = { "meta": { "Nombre": editData.name, "Actualizado": formattedDate} };   
-    
-               jsonData.push(row);
-    
-               let database_ref = editData.database_ref;    
-               
-               if (editLength == countLines) {                               
-                  
-                  datatable.clear();
-                  datatable.rows.add(jsonData);
-                  datatable.draw();    
-               }
-               
-               countLines++;
-           });
-    
-         });     
-         
-       }
+      
     
   //# sourceURL=textedit.js   
 
