1d7061034a17e194969f05ab0828ce8ca76abe4f
developerteam-corp <rohitnchhabra@gmail.com>
2020-06-12 07:49:58 +0530

    app logos on login and logout

--- functions/public/js/logout.js
+++ functions/public/js/logout.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
+  let name = _user.name;
+  let role_desc = _role.desc;
 
-    let name = _user.name;
-    let role_desc = _role.desc;
+  //$('#welcome').html("Bienvenido " +name);
+  //$('#role').html(role_desc);
 
-    //$('#welcome').html("Bienvenido " +name);
-    //$('#role').html(role_desc);
-
-    //# sourceURL=logout.js   
-
+  //# sourceURL=logout.js
+  $("#btn-logout").click(function (e) {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        // Sign-out successful.
+        window.location.href = "index.html";
+      })
+      .catch(function (error) {
+        // An error happened.
+      });
+  });
 });
