faab5dc890c596b9dad9e0846480b1f6f0130312
developerteam-corp <rohitnchhabra@gmail.com>
2020-07-26 20:56:43 +0530

    calendar task

--- functions/public/js/calendar.js
+++ functions/public/js/calendar.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
+  let selectedEvent = "";
+
   getSelectedCalendar("camada");
   $("input[name=options]").change(function () {
     getSelectedCalendar(this.value);
   });
+
   function getSelectedCalendar(type) {
     db.collection("calendar")
       .doc(type)
@@ -12,19 +15,25 @@ $(document).ready(function () {
         var calendarEvents = [];
         response.docs.forEach((doc) => {
           // let doc = change.doc;
+          let id = doc.id;
           let name = doc.data().name;
           let className = doc.data().className;
           let allDay = doc.data().allDay;
           let start = doc.data().start;
-          let date = Date(doc.data().timeStamp);
+          let summary = doc.data().summary;
+          // let date = Date(doc.data().timeStamp);
           //let date = doc.data().timeStamp.toDate();
 
           let event = {
+            id,
             title: name,
             start: formatDate(start.seconds * 1000),
             allDay: allDay,
             className: className,
+            summary,
+            type,
           };
+          console.log(event);
 
           calendarEvents.push(event);
         });
@@ -62,7 +71,6 @@ $(document).ready(function () {
         //   count++;
         // });
       });
-
     function initializeCalendar(events) {
       var calendarEl = document.getElementById("calendar-module");
 
@@ -117,6 +125,30 @@ $(document).ready(function () {
         ],
         dayNamesShort: ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"],
         events: events,
+        eventClick: function (info) {
+          console.log(info.event);
+          selectedEvent = info.event.id;
+          $("#type").prop("disabled", true);
+          $("#name").val(info.event.title);
+          $("#description").val(info.event.extendedProps.summary);
+          $("#type").val(info.event.extendedProps.type);
+          if (info.event.start) {
+            var day = ("0" + info.event.start.getDate()).slice(-2);
+            var month = ("0" + (info.event.start.getMonth() + 1)).slice(-2);
+
+            var today =
+              info.event.start.getFullYear() + "-" + month + "-" + day;
+            $("#start").val(today);
+          }
+          if (info.event.end) {
+            var day = ("0" + info.event.end.getDate()).slice(-2);
+            var month = ("0" + (info.event.end.getMonth() + 1)).slice(-2);
+
+            var today = info.event.end.getFullYear() + "-" + month + "-" + day;
+            $("#end").val(today);
+          }
+          $("#exampleModalLong").modal();
+        },
       });
 
       calendar.render();
@@ -139,21 +171,52 @@ $(document).ready(function () {
     const name = $("#name").val();
     const desc = $("#description").val();
     const type = $("#type").val();
-    const date = $("#date").val();
-    const allday = $("#allday").val();
-
-    db.collection("calendar")
-      .doc(type)
-      .collection(type + "_collection")
-      .add({ name, summary: desc, start: new Date(date), allday })
-      .then((res) => {
-        $("#name").val("");
-        $("#description").val("");
-        // $("#type").val("");
-        $("#date").val("");
-        // $("#allday").val();
-        alert("Event Added");
-      });
+    const start = $("#start").val();
+    const end = $("#end").val();
+    if (!selectedEvent) {
+      db.collection("calendar")
+        .doc(type)
+        .collection(type + "_collection")
+        .add({
+          name,
+          summary: desc,
+          start: new Date(start),
+          end: end ? new Date(end) : null,
+          createdAt: new Date(),
+        })
+        .then((res) => {
+          $("#name").val("");
+          $("#description").val("");
+          $("#start").val("");
+          $("#end").val("");
+          $("#exampleModalLong").modal("toggle");
+          alert("Event Added");
+        });
+    } else {
+      db.collection("calendar")
+        .doc(type)
+        .collection(type + "_collection")
+        .doc(selectedEvent)
+        .set(
+          {
+            name,
+            summary: desc,
+            start: new Date(start),
+            end: end ? new Date(end) : null,
+          },
+          { merge: true }
+        )
+        .then(() => {
+          $("#name").val("");
+          $("#description").val("");
+          $("#start").val("");
+          $("#type").prop("disabled", false);
+          $("#end").val("");
+          $("#exampleModalLong").modal("toggle");
+          selectedEvent = "";
+          alert("Event Updated");
+        });
+    }
   });
 
   //# sourceURL=calendar.js
