fd9825befc32bf21bd56022d3bf9416d16b9a402
Jose <josemanuelvigil@gmail.com>
2021-03-14 03:31:47 +0000

    Finally the authorization steps to get the token and a new refresh_access_tokens field is capable of refreshing the token in order not to authorize any more and get the data from the api.
    Authorize only once?

--- functions/functions/streaming.js
+++ functions/functions/streaming.js
@@ -10,7 +10,7 @@
   var OAuth2 = google.auth.OAuth2;  
 
   //googleapis refresh token
-  https://stackoverflow.com/questions/61204084/nodejs-how-to-get-new-token-with-refresh-token-using-google-api
+  //https://stackoverflow.com/questions/61204084/nodejs-how-to-get-new-token-with-refresh-token-using-google-api
 
   var DeleteCollection = function (path, callback) {
     firestore.collection(path).listDocuments().then(val => {
@@ -35,6 +35,7 @@
       var get_playlists         = newValue.get_playlists;
       var convert_playlists     = newValue.convert_playlists;
       var last_update           = newValue.last_update.toDate();
+      var refresh_access_tokens = newValue.refresh_access_tokens;
 
       var expiry_date;
       if (newValue.tokens) {
@@ -46,6 +47,7 @@
       console.log("fetch_token_trigger: " + fetch_token_trigger);
       console.log("get_playlists: " + get_playlists);
       console.log("convert_playlists: " + convert_playlists);
+      console.log("refresh_access_tokens: " + refresh_access_tokens);
       console.log("last_update: " + last_update);
       console.log("");
 
@@ -241,6 +243,7 @@
 
                   return firestore.collection("streaming").doc("control").set({                   
                     get_playlists: false,
+                    convert_playlists : true,
                     last_update: _time 
                   },{ merge: true });
                   
@@ -259,7 +262,7 @@
       
       if ( fetch_code_trigger ) {        
         
-        var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+        let TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
             process.env.USERPROFILE) + '/.credentials/';
         TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';        
         fs.readFile(client_secret, function processClientSecrets(err, content) {
@@ -270,22 +273,24 @@
           authorize(JSON.parse(content));
         });       
 
-      }         
+      } 
 
-      /*if ( expiry_date && (token_expiry_date_stored !== true)) {
+      if ( refresh_access_tokens ) {        
+        
+        let TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+            process.env.USERPROFILE) + '/.credentials/';
+        TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';        
+        fs.readFile(client_secret, function processClientSecrets(err, content) {
+          if (err) {
+            console.log('Error loading client secret file: ' + err);
+            return;
+          }          
+          let tokens = newValue.tokens;
+          let refreshToken = tokens.refresh_token;
+          refreshAccessTokens(JSON.parse(content), refreshToken);
+        });       
 
-        console.log("::: Updating ::: expiry_date && token_expiry_date_stored==false");
-
-        var d = new Date(expiry_date);
-        var _time =  new Date();
-
-        firestore.collection("streaming").doc("control").set({                   
-          token_expiry_date: d,
-          token_expiry_date_stored: false,
-          last_update: _time 
-        },{ merge: true });
-
-      }*/   
+      } 
 
       if ( convert_playlists ) {  
 
@@ -365,114 +370,115 @@
 
               }).then(async (playlist_results) => {
 
-                    for (var i=0; i<playlists.length; i++) {
+                  console.log("playlist_results::::  " + JSON.stringify(playlist_results));
 
-                      let idp = playlists[i].id;
+                  for (var i=0; i<playlists.length; i++) {
 
-                      firestore.collection("streaming").doc("control").collection("media")
-                      .doc(idp).collection("videos").get()
-                      .then(async (querySnapshotVideo) => {
+                    var idp = playlists[i].uid;
 
-                          var videosLength = querySnapshotVideo.size;
-                          var countVideos = 1;  
+                    console.log("");
+                    console.log("idp: " + idp);                      
 
-                          //console.log("videosLength: " + videosLength);                                        
+                    firestore.collection("streaming").doc("control").collection("media")
+                    .doc(idp).collection("videos").get()
+                    .then(async (querySnapshotVideo) => {
 
-                          querySnapshotVideo.forEach(async (docVideo) => {
+                        var videosLength = querySnapshotVideo.size;
+                        var countVideos = 1;  
 
-                            let docv        = docVideo.data();
-                            var snippetv    = docv.snippet;    
-                            let thumbnailsv = snippetv.thumbnails;
-                            var defaulv     = thumbnailsv.default; 
+                        console.log("videosLength: " + videosLength);                                        
 
-                            let idv           = docv.id;
-                            let title         = snippetv.title;
-                            var description   = "";
+                        querySnapshotVideo.forEach(async (docVideo) => {
 
-                            if (snippetv.description) {
-                                description = snippetv.description;
-                            }
+                          let docv        = docVideo.data();
+                          var snippetv    = docv.snippet;    
+                          let thumbnailsv = snippetv.thumbnails;
+                          var defaulv     = thumbnailsv.default; 
 
-                            let channelId     = snippetv.channelId; 
-                            let localized     = snippetp.localized;  
-                            let channel_title = localized.title;                           
-                            let position      = snippetv.position;              
+                          let idv           = docv.id;
+                          let title         = snippetv.title;
+                          var description   = "";
 
-                            let videoId       = snippetv.resourceId.videoId;
-                            let publishedAt   = snippetv.publishedAt;                            
-                            
-                            let playlistId    = snippetv.playlistId;  
-                            var thumbnailv    = "";
+                          if (snippetv.description) {
+                              description = snippetv.description;
+                          }
 
-                            if (defaulv) {
-                              thumbnailv = defaulv.url;
-                            }
+                          let channelId     = snippetv.channelId; 
+                          //let localized     = snippetv.localized;  
+                          let channel_title = snippetv.channelTitle;                           
+                          let position      = snippetv.position;              
 
-                            //let j  = `{"id":"${idv}","title":"${title}","description":"${description}","channelId":"${channelId}","position":${position},"videoId":"${videoId}","publishedAt":"${publishedAt}","playlistId":"${playlistId}","thumbnail":"${thumbnailv}"}`;                            
+                          let videoId       = snippetv.resourceId.videoId;
+                          let publishedAt   = snippetv.publishedAt;                            
+                          
+                          let playlistId    = snippetv.playlistId;  
+                          var thumbnailv    = "";
 
-                            let k  = `{                              
-                                "id":"${title}", 
-                                "title":"${title}",
-                                "uid":"${idv}", 
-                                "description":"${description}",                               
-                                "channelId":"${channelId}",
-                                "channelTitle":"${channel_title}", 
-                                "position":${position}, 
-                                "videoId":"${videoId}", 
-                                "publishedAt":"${publishedAt}", 
-                                "playlistId":"${playlistId}", 
-                                "thumbnail":"${thumbnailv}"                              
-                               }`;  
+                          console.log("playlistId:" + playlistId);
 
-                            let json_video = JSON.parse( k );                            
+                          if (defaulv) {
+                            thumbnailv = defaulv.url;
+                          }
 
-                            promise_videos.push(
-                              firestore.collection("streaming").doc("control").collection("videos").doc(playlistId).collection("videos").add(json_video)
-                            );                         
+                          //let j  = `{"id":"${idv}","title":"${title}","description":"${description}","channelId":"${channelId}","position":${position},"videoId":"${videoId}","publishedAt":"${publishedAt}","playlistId":"${playlistId}","thumbnail":"${thumbnailv}"}`;                            
 
-                            if ( countVideos === videosLength ) {                                                               
-                                
-                                if ( countPlaylist === playlistLength ) {                                                                                                 
+                          let k  = `{                              
+                              "id":"${idv}", 
+                              "title":"${title}",
+                              "uid":"${idv}", 
+                              "description":"${description}",                               
+                              "channelId":"${channelId}",
+                              "channelTitle":"${channel_title}", 
+                              "position":${position}, 
+                              "videoId":"${videoId}", 
+                              "publishedAt":"${publishedAt}", 
+                              "playlistId":"${playlistId}", 
+                              "thumbnail":"${thumbnailv}"                              
+                             }`;  
 
-                                  console.error("");
-                                  console.error("promise_videos: ", JSON.stringify(promise_videos));
-                                  
-                                  await Promise.all(promise_videos);                                    
+                          let json_video = JSON.parse( k );    
 
-                                  console.log("::: Updating ::: convert_playlists");       
+                          promise_videos.push(
+                            firestore.collection("streaming").doc("control").collection("videos").doc(playlistId).collection("videos").add(json_video)
+                          );                         
 
-                                  var _time =  new Date();
+                          console.log("countVideos: " + countVideos + " videosLength: " + videosLength + " countPlaylist: " + countPlaylist + " playlistLength: " + playlistLength);   
 
-                                  firestore.collection("streaming").doc("control").set({                   
-                                    convert_playlists: false,
-                                    last_update: _time 
-                                  },{ merge: true });    
+                          if ( countVideos === videosLength ) {                                                                                               
+                            if ( countPlaylist === playlistLength ) {
 
-                                  return {};  
-                                  resolve(true);                                
-                                  
-                                }
-                                
-                                countPlaylist++; 
-                                countVideos = 1;
+                              console.log("LLEGA!!!");
 
-                            }                    
-                            
-                            countVideos++;
+                              return Promise.all(promise_videos);                                                        
+                            }                                
+                            countPlaylist++; 
+                            countVideos = 1;
+                          }                                               
+                          countVideos++;
 
-                          });    
+                        });
 
-                          return {};
+                    //end videos     
+                    }).catch((error) => {
+                      console.log('Error saving videos:', error);
+                      return error;
+                    });
+                  // end for
+                  }                    
 
-                      //end videos     
-                      }).catch((error) => {
-                        console.log('Error saving videos:', error);
-                        return error;
-                      });
+              }).then(async (promise_videos_results) => {
 
-                    // end for
-                    }
-                    return {};
+                  console.log("::: Updating ::: convert_playlists");       
+
+                  var _time =  new Date();
+
+                  firestore.collection("streaming").doc("control").set({                   
+                    convert_playlists: false,
+                    last_update: _time 
+                  },{ merge: true });    
+
+                  resolve(true); 
+                  return {};  
 
               // end 
               }).catch((error) => {
@@ -530,6 +536,49 @@
       },{ merge: true }).catch((error) => {
         console.log('Error setting collection:', error);
         return error;
+      });
+
+    }
+
+    var refreshAccessTokens = function (credentials, refreshToken) {
+
+      let clientSecret    = credentials.web.client_secret;
+      let clientId        = credentials.web.client_id;
+      let redirectUrl     = credentials.web.redirect_uris[0];
+      var oauth2Client    = new OAuth2(clientId, clientSecret, redirectUrl);
+
+      /*let oauth2Client = new google.auth.OAuth2(
+             secret.clientID,
+             secret.clientSecret,
+             secret.redirectUrls
+      );*/
+
+      console.log("clientSecret: " + clientSecret);
+      console.log("clientId: " + clientId);
+      console.log("redirectUrl: " + redirectUrl);
+      console.log("clientSecret: " + clientSecret);
+      console.log("oauth2Client: " + JSON.stringify(oauth2Client));
+      console.log("");
+
+      oauth2Client.credentials.refresh_token = refreshToken;
+
+      oauth2Client.refreshAccessToken( async (error, tokens) => {
+        
+        var _time =  new Date();
+
+        console.log("::: Refresh ::: refresh_access_tokens");
+
+        console.log("tokens : " + JSON.stringify(tokens));
+
+        firestore.collection("streaming").doc("control").set({ 
+          tokens: tokens,
+          refresh_access_tokens: false,
+          last_update: _time 
+        },{ merge: true }).catch((error) => {
+          console.log('Error setting collection:', error);
+          return error;
+        });
+      
       });
 
     }
